fe43e91eb153f02628687aa589a260e50f3f40e7
Tomas1974 <114471994+Tomas1974@users.noreply.github.com>
2025-07-17 17:10:46 +0200

    Opdater

--- view.py
+++ view.py
@@ -1,9 +1,12 @@
 import taipy.gui.builder as tgb
 import random
+from playsound import playsound
+import pygame
 
+SEJRS_ANTAL = 7
 input_text = ""
 display_text = ""
-image = "bird.jpg"
+image = "billeder\\bird.jpg"
 rigtig_forkert = ""
 
 data = {
@@ -12,40 +15,56 @@ data = {
 
 }
 
-tal = random.randint(1,100)
-print(tal)
+tal = 0
 counter = 0
 gennemløb = 0
 
+
+def play_music(mp3File):
+    pygame.mixer.init()
+    pygame.mixer.music.load(mp3File)
+    pygame.mixer.music.play()
 
 def spil(state):
     
     global counter
     
-    if counter == 7:
-           counter = 0
-            
-    
-    
-    if int(state.tal) == int(state.input_text):
-        
-        
-        state.tal = random.randint(1,100)
-        print(state.tal)
-        
-        counter += 1
-        state.rigtig_forkert = f"Rigtigt {counter}"
-        
-        
+    if counter == SEJRS_ANTAL:
+           nyt_spil(state)
+                       
     else:
-        state.rigtig_forkert = f"Forkert"
-        counter = 0
+        
+        if int(state.tal) == int(state.input_text):
+            
+            
+            state.tal = random.randint(1,100)
+            print(state.tal)
+            
+            counter += 1
+            state.rigtig_forkert = f"Rigtigt {counter}"
+            play_music("lyd\\correct.mp3")
+            state.image = "billeder\\rigtig.gif"
+            
 
-    
-    if counter == 7:
-        state.rigtig_forkert = f"Du har vundet"
-        state.gennemløb += 1
-        counter = 0
+            
+        else:
+            state.rigtig_forkert = f"Forkert"
+            play_music("lyd\\forkert.mp3")
+            state.image = "billeder\\forkert.gif"
+            counter = 0
+            state.gennemløb = 0
+            
+            
+        
+        if counter == SEJRS_ANTAL:
+            state.rigtig_forkert = f"Du har vundet"
+            play_music("lyd\\finish.mp3")
+            state.image = "billeder\\Finish.png"
+            
+            state.gennemløb += 1
+            
+
+    state.input_text = ""
 
 
 
@@ -58,6 +77,9 @@ def nyt_spil(state):
     print(state.tal)
     state.rigtig_forkert = ""
     counter = 0 
+    state.gennemløb = 0
+    state.image = "billeder\\bird.jpg"
+
 
 
 
@@ -73,8 +95,8 @@ with tgb.Page() as page:
         with tgb.layout("100"):
             with tgb.layout("30 10 20 40"):
                 tgb.input(value = "{input_text}" )
-                tgb.button("Spil", on_action=spil)
-                tgb.button("Gentag", on_action=spil)
+                tgb.button("Svar", on_action=spil)
+                tgb.button("Gentag lyd", on_action=spil)
                 tgb.button("Nyt spil", on_action=nyt_spil)
             
             tgb.text(value="{rigtig_forkert}")
