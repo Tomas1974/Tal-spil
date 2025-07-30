ec2a0fe7cecb4d3264193e912f64d68971316d14
Tomas1974 <114471994+Tomas1974@users.noreply.github.com>
2025-07-30 09:05:21 +0200

    Update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+    
 from taipy import Gui 
 from view import page
-import random
+
     
 
 
 if __name__ == "__main__":
-    Gui(page=page).run(dark_mode=True, use_reloader=True)
+    Gui(page=page).run(dark_mode=True, run_server=False)
