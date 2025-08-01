aeda2c0d2c2a1d52149f940224f42f348b5ac2fb
Tomas1974 <114471994+Tomas1974@users.noreply.github.com>
2025-08-01 13:38:27 +0200

    main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from taipy import Gui
+from taipy import Gui 
 from view import page
 
-gui = Gui(page=page)
 
-# This is what Heroku will call when using a WSGI server like gunicorn
-app = gui.run(dark_mode=True, run_server=True, use_reloader=False, return_flask_app=True)
+if __name__ == "__main__":
+    Gui(page=page).run(dark_mode=True)
+
+
+
